944bd89bc39165f6fb6ff01dfe259137fd93d295
njberman <njberman16@hotmail.com>
2023-02-05 13:18:37 +0000

    Add request to thing

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 
 <script setup>
 import { ref } from 'vue';
+import axios from 'axios';
 import LoginPage from './components/LoginPage.vue';
 import ChatPage from './components/ChatPage.vue';
 
@@ -16,6 +17,8 @@ const loggingIn = ref(true);
 const login = () => {
   loggingIn.value = false;
 };
+
+axios.get('https://school-chat-app-server.onrender.com');
 </script>
 
 <style lang="scss"></style>
