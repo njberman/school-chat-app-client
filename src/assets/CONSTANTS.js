b84096f1d48f3411dc13c1a7293cdcf2e6ab9cd5
njberman <njberman16@hotmail.com>
2023-02-07 19:22:43 +0000

    Add a shit bunch of stuff but cba to write it so meh

--- src/assets/CONSTANTS.js
+++ src/assets/CONSTANTS.js
@@ -1,2 +1,2 @@
-export default 'localhost:6969';
-// export default 'school-chat-app-server.onrender.com';
+// export default 'localhost:6969';
+export default 'school-chat-app-server.onrender.com';
